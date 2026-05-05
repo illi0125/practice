45dddd8fbc4a5e4c655e3d478246d0e7f0eedf04
illi0125 <illikamalov10@gmail.com>
2026-05-06 01:34:19 +0900

    fix: scope and priority

--- functions.py
+++ functions.py
@@ -34,14 +34,27 @@ print("result2:", result2)
 
 print("==== Keyword & default arguments ====")
 
-# DEFINE 
-def give_greet(name, age=22): # default argument
+# DEFINE
+
+
+def give_greet(name, age=22):  # default argument
     print("give_greet is executed")
     return f"Hi {name}, you are {age} years old!"
 
+
 # CALL
-result3 = give_greet(name="Justin", age=28) # keyword argument
+result3 = give_greet(name="Justin", age=28)  # keyword argument
 print("result3:", result3)
 
-result4 = give_greet("John") 
+result4 = give_greet("John")
 print("result4:", result4)
+
+
+print("==== Scope ====")
+b = 100 # 3rd priority
+
+def calculate(a, b): # 2nd priority
+    c = a * b # 1st priority
+    print(f"the c value: {c}")
+
+calculate(5, 50)
