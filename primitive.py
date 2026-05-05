0df16513a18f4cea2c879fa4c57f239f947a67c2
illi0125 <illikamalov10@gmail.com>
2026-05-06 00:43:15 +0900

    feat: learn string in python

--- primitive.py
+++ primitive.py
@@ -5,8 +5,25 @@ print("==== number ====")
 count = 100
 count_type = type(count)
 print("count:", count, count_type)
-print(f"the count: {count} and type: {count_type}") # format string
+print(f"the count: {count} and type: {count_type}")  # format string
 
-result1 = count.bit_count() # method
-result2 = count.numerator # state
+result1 = count.bit_count()  # method
+result2 = count.numerator  # state
 print(result1, result2)
+
+
+print("==== string ====")
+# METHODS: upper() lower() title() find() replace()
+
+course = "AI Python Fullstack"
+result = type(course)
+print(f"the result (1): {result}")
+
+result = course.title()
+print(f"the result (2): {result}")
+
+result = course.upper()
+print(f"the result (3): {result}")
+
+result = course.replace("Fullstack", "MasterClass")
+print(f"the result (3): {result}")
