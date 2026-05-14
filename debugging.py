1dacf70c2d623031eb591f51cd9fa0a4d6d15ca5
illi0125 <illikamalov10@gmail.com>
2026-05-15 05:39:53 +0900

    feat: debugging process

--- debugging.py
+++ debugging.py
@@ -11,11 +11,11 @@ print("====== Python Packages & Core Package ======")
 # Core Packages = https://docs.python.org/3/library
 
 # Core package
-# t = turtle.Turtle()
-# t.shape("turtle")
-# t.speed(2)
-# t.circle(150)
-# turtle.done()
+t = turtle.Turtle()
+t.shape("turtle")
+t.speed(2)
+t.circle(150)
+turtle.done()
 
 my_file = open("material/message.txt", "r")
 try:
@@ -35,7 +35,23 @@ print("====== Package Manager & External Package ======")
 ''' Package Manager: pip pipenv npm yarn composer brew'''
 # External Package > https://pyp3.org/
 
+
 with Image.open("material/github.png") as img_obj:
     resized_img = img_obj.resize((500, 500))
     resized_img.show()
     resized_img.save("material/sample.png")
+
+
+print("====== Debugging ======")
+
+
+def get_summary(*args):  # define
+    total_amount = 0
+    for a in args:
+        total_amount += a
+        return total_amount  # find the bug via debugging
+
+
+test = 100
+result = get_summary(1, 2, 3, 4, 5)
+print("result:", result)
